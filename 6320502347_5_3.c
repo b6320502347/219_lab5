c376155689031d6ce3cd3926334b278bcfbec854
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-11 15:50:45 +0700

    ..

--- 6320502347_5_3.c
+++ 6320502347_5_3.c
@@ -2,22 +2,19 @@
 int main(){
     int n,i,j,x=1;
     scanf("%d",&n);
-        for(i=2;x<n;i++){
-            j=i;
-            while(1){
+    for(i=2;x<n;i++)
+            for(j=i;;)
             if(j%2==0)
                 j/=2;
              else if(j%3==0)
                  j/=3;
              else if(j%5==0)
                  j/=5;
-            else break;
+            else {
+                if(j<=5)
+                    x++;
+                break;
                  }
-    printf("%d %d\n",x,i);
-    if(j<=5){
-        x++;
-    }
-    }
         if(x==1)
         printf("%d",x);
         else printf("%d",i-1);
