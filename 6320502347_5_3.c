fa0af2ec2b2e0b6198972ffc397c400fa775dd35
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-11 15:23:35 +0700

    ...

--- 6320502347_5_3.c
+++ 6320502347_5_3.c
@@ -2,11 +2,13 @@
 int main(){
     int n,i,x=1;
     scanf("%d",&n);
-    for(i=2;x<=n;i++){
-        if(i%2==0||i%3==0||i%5==0)
+    for(i=2;x<n;i++){
+        if((i%2==0||i%3==0||i%5==0)&&i%7!=0){
             x++;
+            }
+        printf("%d %d\n",x,i);
     }
         if(x==1)
         printf("%d",x);
-        else printf("%d",i);
+        else printf("%d",i-1);
 }
