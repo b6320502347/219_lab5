a327a57d872ec072f71b96efd411208f7bf321b9
b6320502347 <78533381+b6320502347@users.noreply.github.com>
2021-03-11 18:07:05 +0700

    ...

--- 6320502347_5_2.c
+++ 6320502347_5_2.c
@@ -13,5 +13,18 @@ int main(){
             else printf("*");
             }
             printf("\n");
+            }
+        for(k=k-2,i=(n+1)/2;i>=1;i--,k-=2){
+        for(j=m;j>=1;j--){
+             if(n%2!=0&&i==(n+1)/2)
+            break;
+            if(j!=x+i&&j!=x+i-k)
+            printf("_");
+            else printf("*");
+            }
+         if(n%2!=0&&i==(n+1)/2);
+         else
+            printf("\n");
 }
+
 }
